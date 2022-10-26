544db84ced342aebfcb781c59f867d7904f43385
inhan <hunt24333@gmail.com>
2022-10-27 07:34:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind CSS Tutorial
+
+tailwind css 를 익히기 위한 튜토리얼입니다
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    port: "auto",
+    open: true,
+  },
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./htmlTemplate/index.html",
+    }),
+  ],
+};
